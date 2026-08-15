b3a80da1b395741d9aa4c141a5819dcf669d9070
vladislav-koval <54426581+vladislav-koval@users.noreply.github.com>
2026-08-15 20:26:45 +0700

    Feature/prepare deployment (#17)
    * add version routes
    
    * add deploy

--- internal/platform/transport/http/middleware/common.go
+++ internal/platform/transport/http/middleware/common.go
@@ -26,6 +26,7 @@ func CORS(allowedOriginsList []string) Middleware {
 				w.Header().Set("Access-Control-Allow-Origin", origin)
 				w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
 				w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+				w.Header().Set("Access-Control-Allow-Credentials", "true")
 			}
 
 			if r.Method == http.MethodOptions {
